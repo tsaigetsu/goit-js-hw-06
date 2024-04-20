445ce14906c9b06c22640a10c1a5e857c2838f3a
Vladyslav Aka Tsaigetsu <callmepempai@gmail.com>
2024-04-20 15:36:35 +0200

    hw 1

--- js/task-2.js
+++ js/task-2.js
@@ -1,42 +1,36 @@
-const getUsersWithFriend = (users, friendName) => {
-  return users.filter(user => user.friends.includes(friendName)).map(user => user.name);
-};
+console.log('task 2');
 
-const allUsers = [
-  {
-    name: "Moore Hensley",
-    friends: ["Sharron Pace"]
-  },
-  {
-    name: "Sharlene Bush",
-    friends: ["Briana Decker", "Sharron Pace"]
-  },
-  {
-    name: "Ross Vazquez",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
-  },
-  {
-    name: "Elma Head",
-    friends: ["Goldie Gentry", "Aisha Tran"]
-  },
-  {
-    name: "Carey Barr",
-    friends: ["Jordan Sampson", "Eddie Strong"]
-  },
-  {
-    name: "Blackburn Dotson",
-    friends: ["Jacklyn Lucas", "Linda Chapman"]
-  },
-  {
-    name: "Sheree Anthony",
-    friends: ["Goldie Gentry", "Briana Decker"]
+
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
   }
-];
 
-console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
-// ["Sharlene Bush", "Sheree Anthony"]
+  getItems() {
+    return this.#items;
+  }
 
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-// ["Elma Head", "Sheree Anthony"]
+  addItem(newItem) {
+    return this.#items.push(newItem);
+  }
 
-console.log(getUsersWithFriend(allUsers, "Adrian Cross")); // []
+  removeItem(itemToRemove) {
+    const removedItem = this.#items.indexOf(itemToRemove);
+    return this.#items.splice(removedItem, 1);
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
